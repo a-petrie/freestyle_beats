9887edae5cbcc3484e84fa4a739a92c34c431bf3
a-petrie <allantpetrie@googlemail.com>
2022-01-16 13:33:10 +0000

    modifying beatdownload to always pull from git before downloading

--- beatdownload.sh
+++ beatdownload.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+git pull origin main
+
 touch .downloaded
 diff .yturls .downloaded | awk 'NR>1{ print $2 }' | xargs -d"\n" youtube-dl -x --audio-format mp3
 diff .yturls .downloaded | awk 'NR>1{ print $2 }' >> .downloaded
